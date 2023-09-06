c760d3936a5722f12c1c1973e16cc5ee601aa236
Dell-User <Dell-User@DESKTOP-HCCU5E8>
2023-09-06 04:58:34 +0200

    Add project files.

---
+++ IntroTolistBox/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ListBox1 = New ListBox()
+        Label1 = New Label()
+        btnYourGender = New Button()
+        SuspendLayout()
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 25
+        ListBox1.Items.AddRange(New Object() {"Male", "Female", "Other"})
+        ListBox1.Location = New Point(434, 115)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(180, 129)
+        ListBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(311, 118)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(69, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Gender"
+        ' 
+        ' btnYourGender
+        ' 
+        btnYourGender.Location = New Point(434, 265)
+        btnYourGender.Name = "btnYourGender"
+        btnYourGender.Size = New Size(189, 105)
+        btnYourGender.TabIndex = 2
+        btnYourGender.Text = "Your"
+        btnYourGender.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnYourGender)
+        Controls.Add(Label1)
+        Controls.Add(ListBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnYourGender As Button
+End Class
---
+++ IntroTolistBox/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btnYourGender_Click(sender As Object, e As EventArgs) Handles btnYourGender.Click
+        Dim stYourGender As String
+
+        stYourGender = ListBox1.SelectedItem
+        MsgBox("you are " & stYourGender)
+
+    End Sub
+End Class
